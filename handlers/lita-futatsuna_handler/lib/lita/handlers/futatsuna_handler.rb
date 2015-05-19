62a2ce76a811c009951f8a1986cdb84b9a34be46
yuta25 <kawai@pepabo.com>
2015-05-19 18:01:51 +0900

    fetch random word from wikipedia if word wasn't given

--- handlers/lita-futatsuna_handler/lib/lita/handlers/futatsuna_handler.rb
+++ handlers/lita-futatsuna_handler/lib/lita/handlers/futatsuna_handler.rb
@@ -1,15 +1,30 @@
 # -*- coding: utf-8 -*-
 require "mecab"
+require "json"
 
 module Lita
   module Handlers
     class FutatsunaHandler < Handler
-      route /二つ名[ 　]+(.+)[ 　]+(.+)/, :make_futatsuna, help: { "二つ名 foo bar" => "Makes futatsuna with foo and bar." }
+      route /二つ名(.*)/, :make_futatsuna, help: { "二つ名 foo bar" => "Makes futatsuna with foo and bar." }
 
       def make_futatsuna(response)
-        first = word = response.matches[0][0]
-        second = word = response.matches[0][1]
-
+        explain = true
+        matches = response.matches[0][0].split(/[ 　]/)
+        matches.shift
+        matches.delete(nil)
+        if matches.length == 0
+          first = word = random_word_from_wikipedia[0]
+          second = word = mahjong_word_from_wikipedia
+          first, second = second, first if (rand(2) == 0)
+        elsif matches.length == 1
+          first = word = matches[0]
+          second = word = mahjong_word_from_wikipedia
+          first, second = second, first if (rand(2) == 0)
+        elsif matches.length >= 2
+          first = word = matches[0]
+          second = word = matches[1]
+          explain = false
+        end
         w1 = group_by_clause(first).map(&:join)
         w2 = group_by_clause(second).map(&:join)
 
@@ -19,6 +34,7 @@ module Lita
         w2_index = 1 if w2.size <= 2
 
         response.reply (w1[0..w1_index] + w2[w2_index..-1]).join
+        response.reply ("( #{first} + #{second} )") if explain
       end
 
       class Word < Struct.new(:word, :parts)
@@ -71,6 +87,20 @@ module Lita
         end
 
         return a
+      end
+
+      def random_word_from_wikipedia(num=1)
+        uri = URI.parse("http://ja.wikipedia.org/w/api.php?action=query&format=json&rnnamespace=0&list=random&rnlimit=#{num}")
+        json = Net::HTTP.get(uri)
+        result = JSON.parse(json)["query"]["random"].map{|a| a["title"].force_encoding("utf-8")}
+      end
+
+      def mahjong_word_from_wikipedia
+        uri = URI.parse("http://tools.wmflabs.org/erwin85/randomarticle.php?lang=ja&family=wikipedia&categories=%E9%BA%BB%E9%9B%80&namespaces=0&subcats=1&d=9")
+        res = Net::HTTP.get_response(uri)
+        res = Net::HTTP.get(URI.parse('http:' + res['location'].gsub("w/index.php?title=:", "wiki/")))
+        res =~ /<title>(.+)<\/title>/
+        $1.to_s.gsub(/ \- Wikipedia$/, '').force_encoding("utf-8")
       end
     end
 
